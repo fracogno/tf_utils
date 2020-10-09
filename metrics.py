Normalization + kernel regularizer + GDICEL fix

metrics.py
@@ -13,20 +13,20 @@ class MetricsManager:
 
     def generalize_dice_loss(self, one_hot, logits):
         w = tf.reduce_sum(one_hot, axis=[1, 2, 3])
-        w = 1 / (w + 1e-6)
-        w = w / tf.reduce_sum(w)  # Normalize weights
+        w = 1 / (w ** 2 + 1e-6)
+        # w = w / tf.reduce_sum(w)  # Normalize weights
 
         probs = tf.nn.softmax(logits)
 
         multed = tf.reduce_sum(probs * one_hot, axis=[1, 2, 3])
-        summed = tf.reduce_sum(probs + one_hot, axis=[1, 2, 3])
+        summed = tf.reduce_sum(probs, axis=[1, 2, 3]) + tf.reduce_sum(one_hot, axis=[1, 2, 3])
 
-        numerator = tf.reduce_sum(w * multed, axis=-1)
-        denominator = tf.reduce_sum(w * summed, axis=-1)
+        numerator = w * multed
+        denominator = w * summed
 
-        dice_score = tf.reduce_mean(2. * numerator / (denominator + 1e-6))
+        dice_score = tf.reduce_mean(2. * numerator / (denominator + 1e-6), axis=0)
 
-        return 1. - dice_score
+        return 1. - tf.reduce_mean(dice_score)
 
     def dice_score_from_logits(self, one_hot, logits):
         """

nets/unet.py
@@ -4,13 +4,29 @@ from tf_utils.blocks.cnn_block import CNN
 
 
 class UNet(tf.keras.Model):
-    def __init__(self, n_classes, n_layers, starting_filters, k_size, init, batch_norm, dropout, activation, conv_per_layer, max_pool, upsampling, max_n_filters=512):
+    def __init__(self, n_classes, n_layers, starting_filters, k_size, init, batch_norm, dropout, activation, conv_per_layer, max_pool,
+                 upsampling, kernel_regularizer, dataset_train, max_n_filters=512):
         super(UNet, self).__init__()
 
         self.n_layers = n_layers
         self.conv_per_layer = conv_per_layer
         self.max_pool = max_pool
         self.upsampling = upsampling
+        self.normalization = dataset_train is not None
+
+        # Normalization layer
+        if self.normalization:
+            self.normalizer = tf.keras.layers.experimental.preprocessing.Normalization()
+            dataset = dataset_train.map(lambda x: x["X"])
+            self.normalizer.adapt(dataset)
+
+        if kernel_regularizer is not None:
+            if kernel_regularizer[0] == "L1":
+                kernel_regularizer = tf.keras.regularizers.L1(kernel_regularizer[1])
+            elif kernel_regularizer[0] == "L2":
+                kernel_regularizer = tf.keras.regularizers.L2(kernel_regularizer[1])
+            else:
+                raise NotImplementedError(kernel_regularizer)
 
         self.encoder = []
         for i in range(n_layers):
@@ -26,7 +42,8 @@ class UNet(tf.keras.Model):
             # How many CNN layers at each stage
             for j in range(conv_per_layer):
                 strides = 2 if j == 0 and i != 0 and max_pool is False else 1
-                self.encoder.append(CNN(n_filters, k_size, strides=strides, kernel_initializer=init, batch_norm=is_batch_norm, dropout=False, activation=activation))
+                self.encoder.append(CNN(n_filters, k_size, strides=strides, kernel_initializer=init, batch_norm=is_batch_norm, dropout=False,
+                                        activation=activation, kernel_regularizer=kernel_regularizer))
 
         self.decoder = []
         for i in range(n_layers - 2, -1, -1):
@@ -37,11 +54,16 @@ class UNet(tf.keras.Model):
 
             for j in range(conv_per_layer):
                 strides = 2 if j == 0 and upsampling is False else 1
-                self.decoder.append(CNN(n_filters, k_size, strides=strides, kernel_initializer=init, batch_norm=batch_norm, dropout=dropout, activation=activation, up=True))
+                self.decoder.append(CNN(n_filters, k_size, strides=strides, kernel_initializer=init, batch_norm=batch_norm, dropout=dropout, activation=activation,
+                                        kernel_regularizer=kernel_regularizer, up=True))
 
         self.last_conv = CNN(n_classes, 3, kernel_initializer=init, batch_norm=None, dropout=0., activation=None)
 
     def call(self, x, training):
+
+        if self.normalization:
+            x = self.normalizer(x)
+
         """
             Encoder
         """