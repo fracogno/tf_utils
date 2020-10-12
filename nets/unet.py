Fix normalization layer

nets/unet.py
@@ -5,7 +5,7 @@ from tf_utils.blocks.cnn_block import CNN
 
 class UNet(tf.keras.Model):
     def __init__(self, n_classes, n_layers, starting_filters, k_size, init, batch_norm, dropout, activation, conv_per_layer, max_pool,
-                 upsampling, kernel_regularizer, dataset_train, max_n_filters=512):
+                 upsampling, kernel_regularizer, dataset_train, max_n_filters=512, is_predicting=False):
         super(UNet, self).__init__()
 
         self.n_layers = n_layers
@@ -17,8 +17,11 @@ class UNet(tf.keras.Model):
         # Normalization layer
         if self.normalization:
             self.normalizer = tf.keras.layers.experimental.preprocessing.Normalization()
-            dataset = dataset_train.map(lambda x: x["X"])
-            self.normalizer.adapt(dataset)
+
+            # Adapt only during training, otherwise load weights
+            if not is_predicting:
+                dataset = dataset_train.map(lambda x: x["X"])
+                self.normalizer.adapt(dataset)
 
         if kernel_regularizer is not None:
             if kernel_regularizer[0] == "L1":